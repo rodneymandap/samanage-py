6087ce24dc8c0c36428d7448bde7db14316fc926
rodney <rodneyjan.mandap@iwgplc.com>
2020-10-12 21:08:37 +0800

    Separate about file to easily change in the future

--- setup.py
+++ setup.py
@@ -1,21 +1,29 @@
+import os
 import setuptools
+
+dito = os.path.abspath(os.path.dirname(__file__))
+
+about = {}
+with open(os.path.join(dito, 'samanage', '__version__.py'), 'r', 'utf-8') as f:
+    exec(f.read(), about)
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="samanage",
-    version="0.0.1",
-    author="Rodney Jan Mandap",
-    author_email="rodneyjan.mandap@iwgplc.com",
-    description="This is a package to easily call request to Samanage API",
+    name=about['__title__'],
+    version=about['__version__'],
+    author=about['__about__'],
+    author_email=about['__author_email__'],
+    description=about['__description__'],
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/rodneymandap/iwg-samanage",
+    url=about['__url__'],
     packages=setuptools.find_packages(),
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
+        "Natural Language :: English",
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
