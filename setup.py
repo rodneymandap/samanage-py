86e791d0dbac5cf71ae9c4549dbbd0337e6dffd9
rodney <rodneyjan.mandap@iwgplc.com>
2020-10-13 16:16:30 +0800

    Add encoding='utf-8' when reading __version__.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 dito = os.path.abspath(os.path.dirname(__file__))
 
 about = {}
-with open(os.path.join(dito, 'samanage', '__version__.py'), 'r', 'utf-8') as f:
+with open(os.path.join(dito, 'samanage', '__version__.py'), 'r', encoding='utf-8') as f:
     exec(f.read(), about)
 
 with open("README.md", "r") as fh:
@@ -13,13 +13,14 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name=about['__title__'],
     version=about['__version__'],
-    author=about['__about__'],
+    author=about['__author__'],
     author_email=about['__author_email__'],
     description=about['__description__'],
     long_description=long_description,
     long_description_content_type="text/markdown",
     url=about['__url__'],
     packages=setuptools.find_packages(),
+    include_package_data=True,
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
