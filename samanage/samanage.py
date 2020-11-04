732ce0956114eb67440fd9a1ba5f730be70f589c
rodney <rodneyjan.mandap@iwgplc.com>
2020-11-04 17:16:55 +0800

    Add base_url attribute

--- samanage/samanage.py
+++ samanage/samanage.py
@@ -9,21 +9,24 @@ class Samanage:
 
     connection_constructor = Connection
 
-    def __init__(self, token, **kwargs):
+    def __init__(self, token, base_url=None, **kwargs):
 
         headers = kwargs.get('headers', None)
         if token:
             if headers is None:
-                self.headers = {
+                kwargs['headers'] = {
                     'Accept': f'application/vnd.samanage.{SAMANAGE_API_VERSION}+json',
                     'Content-Type': 'application/json',
                     'X-Samanage-Authorization': f'Bearer {token}',
                 }
-            self.con = Connection(token, **kwargs)
+            self.con = self.connection_constructor(token, **kwargs)
         else:
             raise ValueError("Token must be provided.") 
 
-        self._base_url = 'https://api.samanage.com'
+        if base_url is None:
+            self.base_url = 'https://api.samanage.com'
+        else:
+            self.base_url = base_url
 
     def incident(self, **kwargs):
         from .incidents import Incident
