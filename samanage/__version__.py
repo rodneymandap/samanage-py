aaac87a75c39ace6d8577326731217f6800b9ffa
rodney <rodneyjan.mandap@iwgplc.com>
2020-11-24 12:07:35 +0800

    Change version to 0.0.2

--- samanage/__version__.py
+++ samanage/__version__.py
@@ -1,7 +1,7 @@
 __title__ = 'samanage-py'
 __description__ = 'A package to easily connect to Samanage API' 
 __url__ = 'https://github.com/rodneymandap/samanage-py'
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __author__ = 'Rodney Jan Mandap'
 __author_email__ = 'rodneyjan.mandap@gmail.com'
 __copyright__ = 'Copyright (c) 2020 Rodney Jan Mandap'
