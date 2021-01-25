18f4661994089934320c3ef05713103901ddb5a6
rodney <rodneyjan.mandap@iwgplc.com>
2021-01-25 16:10:26 +0800

    Add create method

--- samanage/sites.py
+++ samanage/sites.py
@@ -8,6 +8,10 @@ class Site:
         'sites': '/sites.json',
     }
 
+    _required_fields = (
+        'site'
+    )
+
     def __init__(self, parent=None, *args, **kwargs):
         self.con = parent.con
         self.base_url = parent.base_url
@@ -34,6 +38,16 @@ class Site:
             collections.extend(response.json())
 
         return collections
+
+    def create(self, data):
+        path = self.base_url + self._endpoints.get('sites')
+        if isinstance(data, dict):
+            if 'site' in data:
+                if self._required_fields not in data.get('site'):
+                    raise ValueError('Fields: ' + ', '.join(self._required_fields) + ' is required.')
+        else:
+            raise TypeError('Payload must be a dict.')
+        return self.con.create(path, data=data)
 
     def update(self, id, payload):
         path = self.base_url + self._endpoints.get('site').format(id=id)
